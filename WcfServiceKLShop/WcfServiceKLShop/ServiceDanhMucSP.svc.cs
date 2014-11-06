39e59fd709c4110656132b84766afb46ad3572ce
= <=>
2014-11-06 12:40:22 +0700

    version 1

---
+++ WcfServiceKLShop/WcfServiceKLShop/ServiceDanhMucSP.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceKLShop
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceDanhMucSP" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceDanhMucSP.svc or ServiceDanhMucSP.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceDanhMucSP : IServiceDanhMucSP
+    {
+        private KLShopEntities db = new KLShopEntities();
+        public List<DanhMucSP> All()
+        {
+            return this.db.DanhMucSPs.ToList();
+        }
+    }
+}
